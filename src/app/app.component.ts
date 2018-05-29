911040b2eb740632e3488b43383e8d543ce70452
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-05-29 16:00:58 -0300

    Cap3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  listaFotos
+
+  constructor(conexaoApi: HttpClient){
+
+    conexaoApi.get('http://localhost:3000/v1/fotos')
+              .subscribe(
+                fotosApi => this.listaFotos = fotosApi
+                , erro => console.log(erro)
+              )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { FotoModule } from "./foto/foto.module";
@@ -10,6 +11,7 @@ import { FotoModule } from "./foto/foto.module";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FotoModule
   ],
   providers: [],
